7d71dbea73a7b2f00753a8c27e374d313a17553d
Neha Chandran <67389701+mastermercury18@users.noreply.github.com>
2025-06-26 23:54:51 -0400

    vacuum state

---
+++ strawberry-fields/vacuum_state.py
@@ -0,0 +1,25 @@
+import strawberryfields as sf
+from strawberryfields.ops import *
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+prog = sf.Program(1)
+
+with prog.context as q:
+    Vac | q[0]
+
+eng = sf.Engine('gaussian')
+state = eng.run(prog).state
+
+fig = plt.figure()
+X = np.linspace(-5, 5, 100)
+P = np.linspace(-5, 5, 100)
+Z = state.wigner(0, X, P)
+X, P = np.meshgrid(X, P)
+ax = fig.add_subplot(111, projection="3d")
+ax.plot_surface(X, P, Z, cmap="RdYlGn", lw=0.5, rstride=1, cstride=1)
+fig.set_size_inches(4.8, 5)
+ax.set_axis_off()
+
